2f3f390b365037b2a9c784ba328a97a7d2466215
yunzhi123 <hy48257@gmail.com>
2026-05-24 19:30:03 +0800

    upload lab05

---
+++ Lab05-Multi-Priority-Core/Project/Objects/main.d
@@ -0,0 +1,11 @@
+.\objects\main.o: ..\User\main.c
+.\objects\main.o: ..\freertos\Source\include\FreeRTOS.h
+.\objects\main.o: ..\freertos\Source\include\FreeRTOSConfig.h
+.\objects\main.o: ..\freertos\Source\include\projdefs.h
+.\objects\main.o: ..\freertos\Source\include\portable.h
+.\objects\main.o: ..\freertos\Source\include\portmacro.h
+.\objects\main.o: C:\keil5\core\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: C:\keil5\core\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\main.o: ..\freertos\Source\include\list.h
+.\objects\main.o: ..\freertos\Source\include\FreeRTOS.h
+.\objects\main.o: ..\freertos\Source\include\task.h
